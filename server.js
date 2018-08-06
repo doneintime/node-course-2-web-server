d2d3d8ce59dc4ca21be8b3e2fe941c79ceff0fc1
IvanK <kyselovivan@gmail.com>
2018-08-06 11:31:17 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper('getGrreting', (text)=>{
+    return text.toUpperCase();
+})
+
+app.use((req, res, next)=>{
+    res.render('maitenance.hbs');
+})
+
+app.get('/', (req, res) => {
+    res.render("home.hbs", {
+        homePage: "Home page",
+        greeting: "Welcome to our new website:) hohoh"
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs');
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help.hbs');
+})
+
+app.listen(3000);
