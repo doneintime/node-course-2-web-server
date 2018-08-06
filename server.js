7add04abf7367ba941ea1f81cad1f913bf73fb8e
IvanK <kyselovivan@gmail.com>
2018-08-06 13:06:34 +0300

    Setup for geroku and port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+let port = (process.env.PORT || 3000);
+
 hbs.registerPartials(__dirname + "/views/partials");
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
@@ -9,9 +11,9 @@ hbs.registerHelper('getGrreting', (text)=>{
     return text.toUpperCase();
 })
 
-app.use((req, res, next)=>{
-    res.render('maitenance.hbs');
-})
+// app.use((req, res, next)=>{
+//     res.render('maitenance.hbs');
+// })
 
 app.get('/', (req, res) => {
     res.render("home.hbs", {
@@ -34,4 +36,6 @@ app.get('/help', (req, res) => {
     res.render('help.hbs');
 })
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log(`App started on port ${port}`);
+});
